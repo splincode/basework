6ea6eeaef0cfed8e8f9d5bf21089691b325028fb
Maxim Ivanov <omaxphp@yandex.ru>
2016-03-07 01:29:55 +0300

    Update lesson6.c

--- cbase/lesson6.c
+++ cbase/lesson6.c
@@ -3,7 +3,6 @@ int main(){
 	
 	long long int a = 2000000000000; // изменяйте тип и значение
                                     
-    // изменяйте тип
 	for (int i = 0, j = 0; i < sizeof(long long int) * 8; ++i) {
 		if (a & (1 << i)) printf("1"); 
 		else printf("0");
