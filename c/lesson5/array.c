d84fdf7aa97b22ec88afe3ddc32945964af1a392
splincode <omaxphp@yandex.ru>
2016-03-08 00:30:17 +0300

    update

---
+++ c/lesson5/array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	
+	// Celsius
+    int Celsius[5] = {1, 2, 3, 4, 5};
+    
+    // Fahrenheit
+    int Fahrenheit[5] = {
+    	(9/5)*Celsius[0] + 32,
+    	(9/5)*Celsius[1] + 32,
+    	(9/5)*Celsius[2] + 32,
+    	(9/5)*Celsius[3] + 32,
+    	(9/5)*Celsius[4] + 32
+    };
+
+   	// 0 гр., 1, 2, ... 5 = * значение по Фаренгейту
+	printf("%i = %i \n", Celsius[0], Fahrenheit[0]);
+	printf("%i = %i \n", Celsius[1], Fahrenheit[1]);
+	printf("%i = %i \n", Celsius[2], Fahrenheit[2]);
+	printf("%i = %i \n", Celsius[3], Fahrenheit[3]);
+	printf("%i = %i \n", Celsius[4], Fahrenheit[4]);
+	
+	return 0;
+}
---
+++ js/lesson7/hw2/app.js
@@ -0,0 +1,10 @@
+
+	var n = parseFloat(prompt("Введите n: ")) || 0;
+
+	for(var i = 1; i <= n; ++i){
+		if ((i % 5) == 0) alert(i)
+	}
+
+	
+
+	
