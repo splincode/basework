71c60b14b82751b46e65f734348b8836daaa8e38
splincode <omaxphp@yandex.ru>
2016-05-29 23:13:03 +0300

    update

---
+++ c/lesson12/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "mylib/mylib.h"
+
+int main(){
+	
+	printf("max(5, 2) = %i \n", max(5, 2));
+	return 0;
+}
---
+++ c/lesson12/mylib/mylib.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "mylib.h"
+
+void draw() {
+
+}
+
+int max(int a, int b){
+	printf("good day\n");
+	return a >= b ? a : b;
+}
