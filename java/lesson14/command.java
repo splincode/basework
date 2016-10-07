ac0b98581cd5066fbf2f8e117328ede307fe8556
splincode <omaxphp@yandex.ru>
2016-10-07 23:49:03 +0300

    update

---
+++ java/lesson14/command.java
@@ -0,0 +1,14 @@
+class CommandLine {
+	public static void main(String args[]) {
+	
+		String name = args[0];
+		String lastname = args[1];
+		String firstname = args[2];
+
+		System.out.println("Имя: " + name);
+		System.out.println("Фамилия: " + lastname);
+		System.out.println("Отчество: " + firstname);
+
+	}
+
+}
